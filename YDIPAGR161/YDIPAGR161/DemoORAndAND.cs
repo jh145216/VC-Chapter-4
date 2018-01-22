140f186561d1cce2136050346c8b0d713bdb95e7
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-22 14:58:43 -0600

    update 161

---
+++ YDIPAGR161/YDIPAGR161/DemoORAndAND.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace YDIPAGR161
+{
+    class DemoORAndAND
+    {
+        static void Main(string[] args)
+        {
+            const int ZONE1 = 1, ZONE2 = 2;
+            const int LOW_QUANTITY = 10;
+            string inputString;
+            int quantity;
+            int deliveryZone;
+
+            Console.WriteLine("Delivery is free for zone {0} or {1}",
+                        ZONE1, ZONE2);
+        }
+    }
+}
