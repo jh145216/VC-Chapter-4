eea839a2f3dbc4ae6ac88a8bd381bd903cded91b
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-29 14:00:49 -0600

    update YDI

--- YDIPAGR161/YDIPAGR161/DemoORAndAND.cs
+++ YDIPAGR161/YDIPAGR161/DemoORAndAND.cs
@@ -19,6 +19,19 @@ namespace YDIPAGR161
 
             Console.WriteLine("Delivery is free for zone {0} or {1}",
                         ZONE1, ZONE2);
+            Console.WriteLine("when the number of boxes is less than {0}",
+                       LOW_QUANTITY);
+            Console.WriteLine("Enter delivery zone ");
+            inputString = Console.ReadLine();
+            deliveryZone = Convert.ToInt32(inputString);
+            Console.WriteLine("Enter the number of boxes in the shipment");
+            inputString = Console.ReadLine();
+            quantity = Convert.ToInt32(inputString);
+            if ((deliveryZone == ZONE1 || deliveryZone == ZONE2) &&
+                quantity < LOW_QUANTITY);
+            Console.WriteLine("Delivery is free");
+            else
+                Console.WriteLine("A delivery charge applies");
         }
     }
 }
