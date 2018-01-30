33ef4bfb2523881d1e0d5ba656452524185b3de2
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-30 14:47:12 -0600

    update case 1

---
+++ C4 Case 1/C4 Case 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C4_Case_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************************");
+            Console.WriteLine("* The stars shine in Greenville *");
+            Console.WriteLine("**********************************");
+            Console.WriteLine("Each contestant pays a $25 entrance fee");
+            Console.WriteLine("The competition is more than twice as big this year!");
+            Console.WriteLine("The competition is bigger than ever!");
+            Console.WriteLine("A tighter race this year! Come out and cast your vote!");
+        }
+    }
+}
