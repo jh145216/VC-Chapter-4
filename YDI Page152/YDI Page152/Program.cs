929de35e3be64e0df2e7df4ad371e8833f28c741
jh145216 <31322578+jh145216@users.noreply.github.com>
2018-01-30 13:57:53 -0600

    update YDI

--- YDI Page152/YDI Page152/Program.cs
+++ YDI Page152/YDI Page152/Program.cs
@@ -10,6 +10,32 @@ namespace YDI_Page152
     {
         static void Main(string[] args)
         {
+            string numberString;
+            int num1, num2, num3;
+
+            Console.WriteLine("Enter an Intger ");
+            numberString = Console.ReadLine();
+            num1 = Convert.ToInt32(numberString);
+            Console.WriteLine("Enter an Intger ");
+            numberString = Console.ReadLine();
+            num2 = Convert.ToInt32(numberString);
+            Console.WriteLine("Enter an Intger ");
+            numberString = Console.ReadLine();
+            num3 = Convert.ToInt32(numberString);
+
+            if (num1 == num2)
+                if (num1 == num3)
+                    Console.WriteLine("All three numbers are equal");
+                else
+                    Console.WriteLine("First two are equal");
+            else
+                if (num1 == num3)
+                    Console.WriteLine("First and last are equal");
+                else
+                    if (num2 == num3)
+                        Console.WriteLine("Last two are equal");
+                    else
+                        Console.WriteLine("no two numbers are equal");
         }
     }
 }
